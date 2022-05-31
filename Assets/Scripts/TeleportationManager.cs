82b4e3dd72fee1f1890934dd005d51d530fdfaea
Noegnesis <104540327+Noegnesis@users.noreply.github.com>
2022-05-30 18:21:33 -0700

    GotHandTrackingWorkingAgain

--- Assets/Scripts/TeleportationManager.cs
+++ Assets/Scripts/TeleportationManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class TeleportationManager : MonoBehaviour
 {
+    public GameObject player_rig;
+    public GameObject destination;
+
+    private bool is_aiming = false;
+    private GameObject currentDestination;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentDestination = Instantiate(destination, transform.position, Quaternion.identity);
+    }
+
+    public void SwitchAiming() {
+        is_aiming = !is_aiming;
+
+        if(!is_aiming) {
+            currentDestination.SetActive(false);
+        }
+    }
+
+    public void Teleport() {
+        if (is_aiming && currentDestination.activeSelf) {
+            player_rig.transform.position = currentDestination.transform.position;
+            currentDestination.SetActive(false);
+        }
+    }
+
+    private void checkForDestination() {
+        Ray ray = new Ray(transform.position, transform.rotation * Vector3.up);
+
+        RaycastHit hit;
+
+        bool b = Physics.Raycast(ray, out hit, 5, 1 << 3);
+
+        if (b) {
+            currentDestination.transform.position = hit.point;
+            currentDestination.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (is_aiming) {
+            checkForDestination();
+        }
     }
 }
